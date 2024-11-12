b465af247c3ecc040982b29ca604a43f2883f73a
zinovevvs <ziryanov97@gmail.com>
2024-11-12 22:20:32 +0500

    Add files via upload

--- task.py
+++ task.py
@@ -1,24 +1,21 @@
-from fastapi import APIRouter
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, create_engine
+from sqlalchemy.orm import relationship
+from backend.db import Base, engine
 
-router = APIRouter(prefix="/task", tags=["task"])
 
-@router.get("/")
-def all_tasks():
-    pass
+class Task(Base):
+    __tablename__ = 'tasks'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer, default=0)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False, index=True)
+    slug = Column(String, unique=True, index=True)
 
-@router.get("/{task_id}")
-def task_by_id(task_id: int):
-    pass
+    user = relationship("User", back_populates="tasks")
 
-@router.post("/create")
-def create_task():
-    pass
-
-@router.put("/update")
-def update_task():
-    pass
-
-@router.delete("/delete")
-def delete_task():
-    pass
-
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Task.__table__))
+sql_create_table = str(CreateTable(table))
+print(sql_create_table)
--- user.py
+++ user.py
@@ -1,23 +1,15 @@
-from fastapi import APIRouter
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from backend.db import Base
 
-router = APIRouter(prefix="/user", tags=["user"])
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    age = Column(Integer)
+    slug = Column(String, unique=True, index=True)
 
-@router.get("/")
-def all_users():
-    pass
+    tasks = relationship("Task", back_populates="user")
 
-@router.get("/{user_id}")
-def user_by_id(user_id: int):
-    pass
-
-@router.post("/create")
-def create_user():
-    pass
-
-@router.put("/update")
-def update_user():
-    pass
-
-@router.delete("/delete")
-def delete_user():
-    pass
